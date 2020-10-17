980e6e76d3c42a770ddab4328775fe2e1775932b
iren034 <iren034@gmail.com>
2020-10-17 12:04:53 +0800

    Updates

---
+++ precache-manifest.dff3eb748539a424fe9dac4fe545a896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3c0e16a294f11f482606f6143f72d1",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "2bd74a93d36b6595e0cf",
+    "url": "/React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/React/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd74a93d36b6595e0cf",
+    "url": "/React/static/js/main.3d9d58ad.chunk.js"
+  },
+  {
+    "revision": "79458da80355d5a677b7",
+    "url": "/React/static/js/runtime-main.1a456d4b.js"
+  }
+]);
