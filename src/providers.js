74372be89f94737fa24afad4bd2f44a4e3a0f4d5
ihan.chen <ihan.chen@udnshopping.com>
2020-12-27 12:58:11 +0800

    fix: useContext 補充 Context.Consumer 直接取值

--- src/providers.js
+++ src/providers.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import ViewContext, { useViewContextValue } from './content/view';
 
-function Providers(props) { 
+function Providers(props) {
 
   // Init ViewContext: (https://zh-hant.reactjs.org/docs/context.html)
   const viewContextValue = useViewContextValue();
@@ -11,8 +11,12 @@ function Providers(props) {
   const { children } = props;
 
   return (
-      // STEP 2：使用 Context.Provider (承接: /content/view)
-    <ViewContext.Provider value={viewContextValue}>{children}</ViewContext.Provider>
+    // STEP 2：使用 Context.Provider (承接: /content/view)
+    <ViewContext.Provider value={viewContextValue}>
+      {/* // STEP 2-1 使用 Context.Consumer: 可取得 Context 的值, 公司邏輯是在 page 裡使用 isLage 判斷 */}
+      {/* <ViewContext.Consumer>{({ width }) => (` width: ${width} `)}</ViewContext.Consumer> */}
+      {children}
+    </ViewContext.Provider>
   );
 }
 
